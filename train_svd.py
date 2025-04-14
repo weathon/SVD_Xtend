35ccd823a35e1bcb14845343f1a234b4f00536e4
Marshall <wguo6358@gmail.com>
2025-04-14 01:36:10 -0700

    Update train_svd.py

--- train_svd.py
+++ train_svd.py
@@ -1140,7 +1140,7 @@ def main():
                                     width=args.width,
                                     num_frames=num_frames,
                                     decode_chunk_size=8,
-                                    motion_bucket_id=150,
+                                    motion_bucket_id=225,
                                     fps=7,
                                     noise_aug_strength=0.02,
                                     # generator=generator,
